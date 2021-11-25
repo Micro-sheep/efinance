81a700ddaa46b9e2fa0171bcca49abf5d6098026
sheep <micro-sheep@outlook.com>
2021-11-25 21:49:43 +0800

    为文档更换主题，添加对暗黑模式的支持

--- docs/conf.py
+++ docs/conf.py
@@ -60,4 +60,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+
+html_theme = "furo"
