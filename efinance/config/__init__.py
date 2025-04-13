e483c502b1588d09af092f8d541a219974cb2c0d
zhouxinhit <zhouxinhit@126.com>
2025-04-13 21:47:52 +0800

    DATA_DIR 改为环境变量赋值 	modified:   efinance/config/__init__.py

--- efinance/config/__init__.py
+++ efinance/config/__init__.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+import os
 
 HERE = Path(__file__).parent
 # 数据缓存文件存储目录
-DATA_DIR = HERE / "../data"
+# 优先使用环境变量 EFINANCE_DATA_DIR 设置的路径，如未设置则使用默认路径
+DATA_DIR = Path(os.environ.get('EFINANCE_DATA_DIR', HERE / "../data"))
 # 创建数据缓存文件目录
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 # 搜索词缓存位置
