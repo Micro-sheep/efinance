ba45476b23905acb6565960758d64263db547bfc
Robert Tidball <robert.tidball1@gmail.com>
2026-07-10 11:41:37 +1000

    Add FXMacroData macro data helpers

--- efinance/__init__.py
+++ efinance/__init__.py
@@ -3,9 +3,9 @@
 """
 
 __docformat__ = "restructuredtext"
-from efinance.api import bond, fund, futures, stock
+from efinance.api import bond, fund, futures, macro, stock
 
 from efinance import utils
 
 
-__all__ = ["stock", "fund", "bond", "futures", "utils"]
+__all__ = ["stock", "fund", "bond", "futures", "macro", "utils"]
